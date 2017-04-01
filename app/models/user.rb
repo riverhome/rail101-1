e2f08cf21e3e04c447a83cc4ee5ff09b5383dfee
riverhome <liushuijiaxiang@gmail.com>
2017-04-01 23:30:33 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :groups
+has_many :groups_relationships
+has_many :participated_groups, :through => :group_relationships, :source => :group
 has_many :posts
 end
